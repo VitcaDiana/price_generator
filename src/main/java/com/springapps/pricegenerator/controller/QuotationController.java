a7b3fcd5aee7fd9ff3b39b48bf8c58547315aaef
VitcaDiana <diana.ni.vitca@gmail.com>
2024-04-06 15:05:55 +0300

    first commit

---
+++ src/main/java/com/springapps/pricegenerator/controller/QuotationController.java
@@ -0,0 +1,26 @@
+package com.springapps.pricegenerator.controller;
+
+import com.springapps.pricegenerator.model.Quotation;
+import com.springapps.pricegenerator.service.QuotationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/quotation")
+public class QuotationController {
+
+    private QuotationService quotationService;
+
+    @Autowired
+    public QuotationController(QuotationService quotationService) {
+        this.quotationService = quotationService;
+    }
+
+    @PostMapping("/{productId}/{userId}")
+    public Quotation generateQuotation (@PathVariable Long productId, @PathVariable Long userId){
+
+    }
+}
